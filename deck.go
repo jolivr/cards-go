2581d6732c8903c62fd48c90d888c0864032788d
jamesoliver <jamesoliver@protonmail.com>
2018-08-07 09:06:27 -0400

    converted slice to type of deck added print func to deck

--- deck.go
+++ deck.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"fmt"
+)
+
+//Creating new type of 'deck'
+//Slice of string
+
+type deck []string
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
